81979d62e07e462feddf6e7dee87ea2ba7a6013d
Ilia Puzyrev <ipzrev@gmail.com>
2026-09-02 21:36:56 -0700

    wip-27

--- js/script.js
+++ js/script.js
@@ -14,38 +14,13 @@ requestAnimationFrame(raf);
 
 /* sound */
 
-let audioContext;
-
-function playHoverSound() {
-  if (!audioContext) {
-    audioContext = new AudioContext();
-  }
-
-  if (audioContext.state === "suspended") {
-    audioContext.resume();
-  }
-
-  const oscillator = audioContext.createOscillator();
-  const gain = audioContext.createGain();
-
-  oscillator.type = "sine";
-
-  oscillator.frequency.setValueAtTime(700, audioContext.currentTime);
-
-  gain.gain.setValueAtTime(0.015, audioContext.currentTime);
-
-  gain.gain.exponentialRampToValueAtTime(
-    0.001,
-    audioContext.currentTime + 0.06,
-  );
-
-  oscillator.connect(gain);
-  gain.connect(audioContext.destination);
-
-  oscillator.start();
-  oscillator.stop(audioContext.currentTime + 0.06);
-}
+const hoverSound = document.getElementById("hoverSound");
 
 document.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("mouseenter", playHoverSound);
+  link.addEventListener("mouseenter", () => {
+    hoverSound.currentTime = 0;
+    hoverSound.volume = 0.15;
+
+    hoverSound.play().catch(() => {});
+  });
 });
